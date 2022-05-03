ee60797971428017bc7f74f79f02907d814df33e
vladywill <vladyslavmartyniuk13@gmail.com>
2022-05-03 10:25:46 +0000

    Implement the structure of a single movie card styles

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -1,9 +1,10 @@
 const Movie = () => (
-  <div className="shadow-md bg-white p-5">
-    <p>Movie image</p>
-    <h1>Movie Title</h1>
-    <p>Movie description</p>
-    <p>Social media icons</p>
+  <div className="p-5 rounded-md overflow-hidden pb-10">
+    <div className="bg-lime-400 h-40 text-center flex justify-center items-center">
+      <p>Movie image</p>
+    </div>
+    <h1 className="mt-5 text-center font-semibold">Movie Title</h1>
+    <p className="mt-10 text-center text-sm text-gray-500">Movie description</p>
   </div>
 );
 

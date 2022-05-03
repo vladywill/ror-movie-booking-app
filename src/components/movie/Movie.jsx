6766b49bf7da1a3a1cc3eed27c2b0369092e11ed
vladywill <vladyslavmartyniuk13@gmail.com>
2022-05-03 10:46:09 +0000

    Add more spacting between movie image and title

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -9,7 +9,7 @@ const Movie = () => (
     <div className="bg-lime-400 h-40 text-center flex justify-center items-center">
       <p>Movie image</p>
     </div>
-    <h1 className="mt-5 text-center font-semibold">Movie Title</h1>
+    <h1 className="mt-10 text-center font-semibold">Movie Title</h1>
     <p className="mt-10 text-center text-sm text-gray-500">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
       commodo elit nibh.

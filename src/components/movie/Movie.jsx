d48a5485f477904c35b656ccf6bc174f731adea4
vladywill <vladyslavmartyniuk13@gmail.com>
2022-05-04 22:33:41 +0000

    Add reserve button and more styles on a single movie card

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -2,18 +2,22 @@ import {
   TiSocialFacebook,
   TiSocialTwitter,
   TiSocialInstagram,
-} from 'react-icons/ti';
+} from "react-icons/ti";
 
 const Movie = () => (
-  <div className="p-5 rounded-md overflow-hidden pb-10">
-    <div className="bg-lime-400 h-40 text-center flex justify-center items-center">
+  <div className="overflow-hidden pb-10 shadow-lg mx-4">
+    <div className="bg-gray-800 h-56 text-center flex justify-center items-center relative w-full">
       <p>Movie image</p>
+      <button type="button" className="py-3 px-5 rounded-full bg-white absolute bottom-0 right-0 shadow-xl -mb-6 mr-4 text-lg hover:bg-gray-900 hover:text-white hover:border border text-gray-900">Reserve</button>
     </div>
-    <h1 className="mt-10 text-center font-semibold">Movie Title</h1>
-    <p className="mt-10 text-center text-sm text-gray-400">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-      commodo elit nibh.
-    </p>
+
+    <div className="px-3">
+      <h1 className="mt-10 text-center font-semibold">Movie Title</h1>
+      <p className="mt-10 text-center text-sm text-gray-400">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        commodo elit nibh.
+      </p>
+    </div>
     <div className="mt-5 flex justify-center items-center">
       <ul className="flex items-center">
         <li className="w-8 h-8 border border-gray-300 rounded-full flex justify-center items-center">

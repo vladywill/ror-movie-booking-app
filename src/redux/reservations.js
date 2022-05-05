f349260cbe8c23056fa18f1f8e5a934769c8f7b9
vladywill <vladyslavmartyniuk13@gmail.com>
2022-05-05 14:56:08 +0200

    Add reservation entry inputs

---
+++ src/redux/reservations.js
@@ -0,0 +1,4 @@
+const DELETE_RESERVATION = 'reservation/delete_reservation';
+const FETCH_DATA = 'reservations/FETCH_DATA';
+const UPDATE_STATE = 'reservations/UPDATE_STATE';
+const CREATE_RESERVATION = 'reservations/CREATE_RESERVATION';

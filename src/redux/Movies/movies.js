6abcc0947924ea26c6be9f5d97f584b66db946a0
vladywill <vladyslavmartyniuk13@gmail.com>
2022-05-06 13:58:24 +0200

    initiate book movies details

---
+++ src/redux/Movies/movies.js
@@ -0,0 +1,6 @@
+const END_POINT = 'http://localhost:3000';
+const API_ROUTE = '/api/v1/';
+const initialState = [];
+const FETCH_DATA = 'rooms/FETCH_DATA';
+const UPDATE_STATE = 'rooms/UPDATE_STATE';
+const CREATE_ROOM = 'rooms/BOOK_MOVIE';

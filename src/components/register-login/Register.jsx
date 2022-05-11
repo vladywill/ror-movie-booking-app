3f1a88fe03aab56d2a56f2a143ed22c611e50106
vladywill <vladyslavmartyniuk13@gmail.com>
2022-05-11 12:03:12 +0100

    Add form for registeration

--- src/components/register-login/Register.jsx
+++ src/components/register-login/Register.jsx
@@ -7,34 +7,34 @@ const Register = () => (
       <h1 className="text-center font-semibold text-gray-900 uppercase">
         Register
       </h1>
+      <form className="mt-16 px-10">
+        <label htmlFor="username" className="block">
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter username"
+            className="w-full py-3 border px-3 focus:outline-gray-800"
+          />
+        </label>
+        <label htmlFor="password" className="block mt-5">
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            className="w-full py-3 border px-3 focus:outline-gray-800"
+          />
+        </label>
+        <button
+          type="submit"
+          className="bg-gray-900 text-lg py-2 mt-5 w-full text-white mb-3 border-4 hover:border-4 hover:border-gray-800 hover:bg-white hover:text-gray-900"
+        >
+          Register
+        </button>
+        <Link to="/" className="text-blue-600">
+          Login
+        </Link>
+      </form>
     </div>
-    <form className="mt-16 px-10">
-      <label htmlFor="username" className="block">
-        <input
-          type="text"
-          id="username"
-          placeholder="Enter username"
-          className="w-full py-3 border px-3 focus:outline-gray-800"
-        />
-      </label>
-      <label htmlFor="password" className="block mt-5">
-        <input
-          type="password"
-          id="password"
-          placeholder="Enter password"
-          className="w-full py-3 border px-3 focus:outline-gray-800"
-        />
-      </label>
-      <button
-        type="submit"
-        className="bg-gray-900 text-lg py-2 mt-5 w-full text-white mb-3 border-4 hover:border-4 hover:border-gray-800 hover:bg-white hover:text-gray-900"
-      >
-        Register
-      </button>
-      <Link to="/register" className="text-blue-600">
-        Register
-      </Link>
-    </form>
   </div>
 );
 

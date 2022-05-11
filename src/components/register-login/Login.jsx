f8ae6f2695dcbd5c8c1b0d5aedb206aa99c67c21
vladywill <vladyslavmartyniuk13@gmail.com>
2022-05-11 10:33:54 +0000

    Add background image and overlay in css for login

--- src/components/register-login/Login.jsx
+++ src/components/register-login/Login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Login = () => (
-  <div className="w-full h-screen flex justify-center">
-    <div>
+  <div className="w-full h-screen flex justify-center items-center register-login">
+    <div className="w-1/4 h-96 bg-white shadow-xl rounded-md">
       <h1>Login Page</h1>
       <p>Users login page</p>
     </div>

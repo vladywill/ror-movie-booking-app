ffdb78b985c5cd34ca853e0d5f357c552c1a60e2
vladywill <vladyslavmartyniuk13@gmail.com>
2022-05-11 09:17:53 +0000

    Add register and login components

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import LandingPage from './components/LandingPage';
 import Movies from './components/movie/Movies';
 import NavBar from './components/navbar/NavBar';
 import MovieDetails from './components/movie/MovieDetails';
+import Login from './components/register-login/Login';
+import Register from './components/register-login/Register';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:id" element={<MovieDetails />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<LandingPage />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<MovieDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/register-login/Register.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Login = () => (
+  <div>
+    <h1>Register Page</h1>
+    <p>Users login page</p>
+  </div>
+);
+
+export default Login;

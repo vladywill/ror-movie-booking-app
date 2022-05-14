6d86ca542e1d57b29e048c196b8bde7ad561eb25
vladywill <vladyslavmartyniuk13@gmail.com>
2022-05-14 13:57:32 +0100

    Refactor code

--- src/components/movie/AddMovies.jsx
+++ src/components/movie/AddMovies.jsx
@@ -1,128 +1,57 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import NavBar from '../navbar/NavBar';
+import Input from './Input';
 
-const AddMovies = () => (
-  <>
-    <NavBar />
-    <div className="w-full flex justify-center bg-gray-100 py-16">
-      <div className="w-1/2 shadow-md border bg-white pb-12">
-        <div className="py-3 px-8 border-b-2">
-          <h1>Add Movie</h1>
+const AddMovies = () => {
+  const data = ['Title', 'Director', 'Release Date', 'Playing Time', 'Genre', 'Ticket Price', 'Lead cast', 'Country', 'Photo'];
+
+  return (
+
+    <>
+      <NavBar />
+      <div className="w-full flex justify-center bg-gray-100 py-16">
+        <div className="w-1/2 shadow-md border bg-white pb-12">
+          <div className="py-3 px-8 border-b-2">
+            <h1>Add Movie</h1>
+          </div>
+          <form className="grid grid-cols-1 md:grid-cols-2 px-10 mt-12 w-full">
+
+            {data.map((data) => (
+              <div className="mb-4 w-full" key={uuidv4()}>
+                <label htmlFor="title" className="block mr-5">
+                  <span className="block mb-1">{data}</span>
+                  <Input
+                    type="text"
+                    id="title"
+                    className="py-2 px-3 border border-gray-300 w-full"
+                  />
+                </label>
+              </div>
+            ))}
+            <div className="flex mb-4">
+              <label htmlFor="description" className="block w-full">
+                <span className="block mb-1">Description</span>
+                <textarea
+                  type="file"
+                  id="description"
+                  className="py-2 px-3 border border-gray-300 w-full"
+                />
+              </label>
+            </div>
+            <hr />
+            <div className="mt-3 flex justify-end">
+              <button
+                type="submit"
+                className="mt-5 py-3 px-16 bg-lime-500 hover:bg-lime-800 text-white"
+              >
+                Save Movie
+              </button>
+            </div>
+          </form>
         </div>
-        <form className=" px-10 mt-12">
-          <div className="flex mb-4">
-            <label htmlFor="title" className="block mr-5 w-1/2">
-              <span className="block mb-1">Title</span>
-              <input
-                type="text"
-                id="title"
-                className="py-2 px-3 border border-gray-300 w-full"
-                placeholder="Enter movie title"
-              />
-            </label>
-            <label htmlFor="director" className="block w-1/2">
-              <span className="block mb-1">Director</span>
-              <input
-                type="text"
-                id="director"
-                placeholder="Enter director name"
-                className="py-2 px-3 border border-gray-300 w-full"
-              />
-            </label>
-          </div>
-          <div className="flex mb-4">
-            <label htmlFor="releaseDate" className="block mr-5 w-1/2">
-              <span className="block mb-1">Release Date</span>
-              <input
-                type="date"
-                id="releaseDate"
-                className="py-2 px-3 border border-gray-300 w-full"
-              />
-            </label>
-            <label htmlFor="playingTime" className="block w-1/2">
-              <span className="block mb-1">Playing Time</span>
-              <input
-                type="time"
-                id="playingTime"
-                className="py-2 px-3 border border-gray-300 w-full"
-              />
-            </label>
-          </div>
-          <div className="flex mb-4">
-            <label htmlFor="genre" className="block mr-5 w-1/2">
-              <span className="block mb-1">Genre</span>
-              <input
-                type="text"
-                id="genre"
-                className="py-2 px-3 border border-gray-300 w-full"
-                placeholder="Enter movie genre"
-              />
-            </label>
-            <label htmlFor="ticketPrice" className="block w-1/2">
-              <span className="block mb-1">Ticket Price</span>
-              <input
-                type="text"
-                id="ticketPrice"
-                className="py-2 px-3 border border-gray-300 w-full"
-                placeholder="$ 0.0"
-              />
-            </label>
-          </div>
-          <div className="flex mb-4">
-            <label htmlFor="leadCast" className="block mr-5 w-1/2">
-              <span className="block mb-1">Lead Cast</span>
-              <input
-                type="text"
-                id="leadCast"
-                className="py-2 px-3 border border-gray-300 w-full"
-                placeholder="Enter lead cast name"
-              />
-            </label>
-            <label htmlFor="country" className="block w-1/2">
-              <span className="block mb-1">Country</span>
-              <input
-                type="text"
-                id="country"
-                className="py-2 px-3 border border-gray-300 w-full"
-                placeholder="Enter country"
-              />
-            </label>
-          </div>
-          <div className="flex mb-4">
-            <label htmlFor="photo" className="block w-1/2">
-              <span className="block mb-1">Photo</span>
-              <input
-                type="text"
-                id="photo"
-                className="py-2 px-3 border border-gray-300 w-full"
-                placeholder="Enter photo URL"
-              />
-            </label>
-          </div>
-          <div className="flex mb-4">
-            <label htmlFor="description" className="block w-full">
-              <span className="block mb-1">Description</span>
-              <textarea
-                type="file"
-                id="description"
-                className="py-2 px-3 border border-gray-300 w-full"
-                placeholder="Enter description text"
-              />
-            </label>
-          </div>
-          <hr />
-          <div className="mt-3 flex justify-end">
-            <button
-              type="submit"
-              className="mt-5 py-3 px-16 bg-lime-500 hover:bg-lime-800 text-white"
-            >
-              Save Movie
-            </button>
-          </div>
-        </form>
       </div>
-    </div>
-  </>
-);
-
+    </>
+  );
+};
 export default AddMovies;
---
+++ src/components/movie/Input.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable object-curly-newline */
+import React from 'react';
+
+const Input = ({ type, id, className }) => (
+  <input
+    type={type}
+    id={id}
+    className={className}
+  />
+
+);
+
+export default Input;
